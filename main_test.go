f7fc2353909301c0d8cc2e2d9ee0b13e7feb156e
David Sedláček <david.littlefarmer@gmail.com>
2026-01-06 13:07:52 +0100

    succint method arguments

--- main_test.go
+++ main_test.go
@@ -162,6 +162,7 @@ service ExampleService # oof
 @public
    - GetUser   (   header   :    map  <   string   ,   string   >   ,   userID   :    uint64   )   =>   (  code  :   uint32   ,   user  :   User  )
     - FindUser(s :SearchFilter) => (name: string, user: User) ###! last
+	- Updat  eU ser (  User   ,  really   :    bool  )   =>   (  User ,    ok  :   bool )
 
     -    stream    Re cv   (req  :   string   )
 
@@ -255,6 +256,7 @@ service ExampleService # oof
     @public
   - GetUser(header: map<string,string>, userID: uint64) => (code: uint32, user: User)
   - FindUser(s: SearchFilter) => (name: string, user: User) ###! last
+  - UpdateUser(User, really: bool) => (User, ok: bool)
 
   - stream Recv(req: string)
 
